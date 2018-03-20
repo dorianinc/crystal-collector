6ee095f694c27e5db495f52a9822070f20623552
dorianinc <33275910+dorianinc@users.noreply.github.com>
2018-03-20 14:43:58 -0700

    Add files via upload
    completed everything excluding the wins and losses function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,10 +11,6 @@ var score = 0;
 /*=========== Start Game Function ======= */
 $( document ).ready(function(Startgame)
 {
-    goal = 0;
-    wins = 0;
-    losses = 0;
-    score = 0;
     $("#wins").text("wins: " + wins);
     $("#losses").text("losses: " + losses);
         goal = Math.floor(Math.random() * 19) + 101;
@@ -35,36 +31,86 @@ $( document ).ready(function(Startgame)
     score = score + gem1;
     $("#your_score").text(score);
     console.log("your score is: ", score); 
+
+    if (score === goal)
+    {
+    wins++;
+    alert("you win!");
+    }
+
+    else if (score > goal)
+    {
+    losses++;
+    alert("you lose :(");
+    }
+
 });
 $('#gem2').on ('click', function()
 {
     score = score + gem2;
     $("#your_score").text(score);
     console.log("your score is: ", score); 
+
+    if (score === goal)
+    {
+    wins++;
+    alert("you win!");
+    }
+
+    else if (score > goal)
+    {
+    losses++;
+    alert("you lose :(");
+    }
 });
 $('#gem3').on ('click', function()
 {
     score = score + gem3;
     $("#your_score").text(score);
-    console.log("your score is: ", score); 
+    console.log("your score is: ", score);
+    
+    if (score === goal)
+    {
+    wins++;
+    alert("you win!");
+    }
+
+    else if (score > goal)
+    {
+    losses++;
+    alert("you lose :(");
+    }
 });
 $('#gem4').on ('click', function()
 {
     score = score + gem4;
     $("#your_score").text(score);
     console.log("your score is: ", score); 
-});
 
-if (score === goal)
-{
+    if (score === goal)
+    {
     wins++;
     alert("you win!");
-}
+    }
 
 else if (score > goal)
-{
+    {
     losses++;
     alert("you lose :(");
+    }
+});
+
+
+function reset()
+{
+    score = 0;
+    goal = Math.floor(Math.random() * 19) + 101;
+
+    gem1 = Math.floor(Math.random() * 11) + 1;
+    gem2 = Math.floor(Math.random() * 11) + 1;
+    gem3 = Math.floor(Math.random() * 11) + 1;
+    gem4 = Math.floor(Math.random() * 11) + 1;
+    
 }
 
 });
